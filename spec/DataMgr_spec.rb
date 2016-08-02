4e5d3f38b5251c6f45540dea03c773e670a0b264
Kim, Peter <Peter.Kim@concur.com>
2016-08-02 00:02:37 -0400

    Update spec.name

--- spec/DataMgr_spec.rb
+++ spec/DataMgr_spec.rb
@@ -5,7 +5,10 @@ describe DataMgr do
     expect(DataMgr::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'create object DataMgr::Model' do
+
+    obj = DataMgr::DataModel.new('./spec/fixtures/dut.json')
+    expect(obj.is_a?(DataMgr::DataModel))
+   # expect(false).to eq(true)
   end
 end
