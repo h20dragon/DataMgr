0e22d4e21c613bba332fd877bb132cd84c98e8c6
Kim, Peter <Peter.Kim@concur.com>
2016-08-02 00:51:00 -0400

    Added assertions.

--- spec/DataMgr_spec.rb
+++ spec/DataMgr_spec.rb
@@ -8,6 +8,12 @@ describe DataMgr do
   it 'create object DataMgr::Model' do
 
     obj = DataMgr::DataModel.new('./spec/fixtures/dut.json')
+    rc=obj.getDataElement('data(firstname)')
+    puts "=> RC : #{rc}"
+
+    rc=obj.getDataElement('data(address).get(city)')
+    puts "=> city : #{rc}"
+
     expect(obj.is_a?(DataMgr::DataModel))
    # expect(false).to eq(true)
   end
